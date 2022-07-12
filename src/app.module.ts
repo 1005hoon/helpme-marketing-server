00984bfcd5422e918941f31229de5ae5abf80774
hoon <hoonnotes@naver.com>
2022-07-12 10:22:10 +0900

    chore: implement database module for multiple db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       validationSchema: Joi.object({
         PORT: Joi.number(),
         MONGO_CORP_REG: Joi.string().required(),
         MONGO_MKT: Joi.string().required(),
       }),
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
